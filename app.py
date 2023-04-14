d5742887e4ad17f4a884cca0faf4c808551a83ed
Ishita <ishitamandal06@gmail.com>
2023-04-14 16:07:36 +0800

    app

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def register():
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
-    return render_template('register.html', title='Register')
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -85,7 +85,7 @@ def login():
         else:
             flash('Login unsuccessful. Please check username and password')
             return redirect(url_for('login'))
-    return render_template('login.html' , title='Login')
+    return render_template('login.html')
 
 @app.route('/dashboard')
 @login_required
@@ -110,7 +110,7 @@ def create_account():
         db.session.add(account)
         db.session.commit()
         return redirect(url_for('dashboard'))
-    return render_template('create_account.html', title='Create Account')
+    return render_template('create_account.html')
 
 @app.route('/account/<int:id>')
 @login_required
